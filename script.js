89d9cbcc439aa6b38a916468c0ea2a4b18ed5505
Sani <71819991+dzikrullah04@users.noreply.github.com>
2024-10-16 19:44:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.getElementById("redirectForm").addEventListener("submit", function(even
     event.preventDefault();
     const answer = document.getElementById("answer").value.trim().toLowerCase();
     if (answer === "3") {
-        window.location.href = "https://families.google.com/jp/GO_s1ZAO6lnX3Gw9pNO4wUtGH0qHng";
+        window.location.href = "https://families.google.com/jp/X6tvwti-R265y2yfw9xaUpwMBR0BFg";
     }else if ( answer === "2" ){
-        window.location.href = "https://families.google.com/jp/GO_s1ZAO6lnX3Gw9pNO4wUtGH0qHng";
+        window.location.href = "https://families.google.com/jp/EDmUuhoQCFQ5QQVjCUc_ApgpVgMaQw";
     }else if ( answer === "5" ){
-        window.location.href = "https://families.google.com/jp/GO_s1ZAO6lnX3Gw9pNO4wUtGH0qHng";
+        window.location.href = "https://families.google.com/jp/jQYrsuGXLoHwM3W4Q_YKzGHaLd9cCA";
     }else {
         alert("Jawaban salah. Coba lagi!");
     }
