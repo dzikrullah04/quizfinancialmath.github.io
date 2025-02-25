412824ac5647fc6251416aca4cff1d6e50ccd187
Sani <71819991+dzikrullah04@users.noreply.github.com>
2025-02-25 12:06:34 +0700

    update jawaban quiz

--- kuis1/script.js
+++ kuis1/script.js
@@ -1,12 +1,34 @@
 document.getElementById("redirectForm").addEventListener("submit", function(event) {
     event.preventDefault();
     const answer = document.getElementById("answer").value.trim().toLowerCase();
-    if (answer === "3") {
-        window.location.href = "https://families.google.com/jp/X6tvwti-R265y2yfw9xaUpwMBR0BFg";
-    }else if ( answer === "2" ){
-        window.location.href = "https://families.google.com/jp/EDmUuhoQCFQ5QQVjCUc_ApgpVgMaQw";
-    }else if ( answer === "5" ){
-        window.location.href = "https://families.google.com/jp/jQYrsuGXLoHwM3W4Q_YKzGHaLd9cCA";
+    if (answer === "2410631030113") {
+        window.location.href = "https://families.google.com/jp/5yG7VW6tin9w9M_pF0rciUsrtTk76Q";
+    }else if ( answer === "2410631030223" ){
+        window.location.href = "https://families.google.com/jp/3JjWnxqwbtszd0HLq3D_ciT31XKAmA";
+    }else if ( answer === "2410631030220" ){
+        window.location.href = "https://families.google.com/jp/EZtXYVVGxkk2DbBA3SDgfZFhSzCztg";
+    }else if ( answer === "2410631030108" ){
+        window.location.href = "https://families.google.com/jp/1YROk7stEgA5gPd9mtQ1z9-iQQemJw";
+    }else if ( answer === "2410631030128" ){
+        window.location.href = "https://families.google.com/jp/nZTWnyVKgvhHbQam88QefgfRE3thyA";
+    }else if ( answer === "1b" ){
+        window.location.href = "https://families.google.com/jp/_M_MFosd1nG4jOM-JHb-NAz55CsPuw";
+    }else if ( answer === "2c" ){
+        window.location.href = "https://families.google.com/jp/o6POcUeArGE8qJZdxu7QEsFmL-Yf8g";
+    }else if ( answer === "3b" ){
+        window.location.href = "https://families.google.com/jp/AYN5XqLmt6wXnWg--g-qBwmpcYieOw";
+    }else if ( answer === "jawabana" ){
+        window.location.href = "https://families.google.com/jp/iGvIwE44JITTfC2or6Hk9Hy552PkBg";
+    }else if ( answer === "jawaban" ){
+        window.location.href = "https://families.google.com/jp/eT3q6UvCRtLYhulgmooUWrZ20AsiWg";
+    }else if ( answer === "4d" ){
+        window.location.href = "https://www.canva.com/brand/join?token=9TUVAXws9VAycqSVYWS-1Q&referrer=team-invite";
+    }else if ( answer === "5b" ){
+        window.location.href = "https://wa.me/6285174146390?text=hadiah%20apple%20music";
+    }else if ( answer === "jawaban2" ){
+        window.location.href = "https://families.google.com/jp/qwWH3BcOpgK0e5QzPLko78BRXpyQ9g";
+    }else if ( answer === "tama" ){
+        window.location.href = "https://families.google.com/jp/prUKq87dBrmcC7LYqgsGRk85N7gAIQ";
     }else {
         alert("Jawaban salah. Coba lagi!");
     }
